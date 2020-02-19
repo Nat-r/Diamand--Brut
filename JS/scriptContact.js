d5ae28797b0d8c1cec38eb23b41dc4211569a615
Nathan <nrosevegue@edenschool.fr>
2020-02-19 15:24:22 +0100

    Save 15:10

--- JS/scriptContact.js
+++ JS/scriptContact.js
@@ -1,44 +1,81 @@
+var prenomcheck = 0;
+var nomcheck = 0;
+var emailnomcheck = 0;
+var datecheck = 0;
 var phonecheck = 0;
-function checkInput(event) {
+var citycheck = 0;
+var areacheck = 0;
+function prenomInput(event) {
     var prenom = document.getElementById('prenom')
-    var nom = document.getElementById('nom')
-    var email = document.getElementById('email')
-    var phone = document.getElementById('phone')
-    var date = document.getElementById('date')
-    var city = document.getElementById('city')
     if (!prenom.value)
         prenom.classList.add('is-invalid')
-    else if (prenom.value)
+    else if (prenom.value){
         prenom.classList.replace('is-invalid','is-valid')
+
+    }
+}
+
+function nomInput(event) {
+    var nom = document.getElementById('nom')
     if (!nom.value) 
         nom.classList.add('is-invalid')
     else if (nom.value)
         nom.classList.replace('is-invalid','is-valid')
+}
+
+function emailInput(params) {
+    var email = document.getElementById('email')
     if (!email.value)
         email.classList.add('is-invalid')
     else if (email.value)
         email.classList.replace('is-invalid','is-valid')
-    if (!phone.value)
-        phone.classList.add('is-invalid')
-    else if (phone.value.length == 9){
-        phone.classList.replace('is-invalid','is-valid')
-        //phonecheck = true;
-    }else if (phone.value.length != 9){
-        phone.classList.replace('is-valid','is-invalid')
-        //phonecheck = false;
-    }/*if (!date.value) 
+}
+
+function dateInput(params) {
+    var date = document.getElementById('date')
+    if (!date.value) 
         date.classList.add('is-invalid')
     else if (date.value)
         date.classList.replace('is-invalid','is-valid')
-    */if (!city.value)
+}
+
+function phoneInput(event) {
+    var phone = document.getElementById('phone')
+    if (!phone.value){
+        phone.classList.add('is-invalid')
+    }else if (phone.value.length == 9){
+        phone.classList.replace('is-invalid','is-valid')
+        phonecheck = true;
+    }else if (phone.value.length != 9){
+        phone.classList.replace('is-valid','is-invalid')
+        phonecheck = false;
+    }
+}
+
+function cityInput(event) {
+    var city = document.getElementById('city')
+    if (!city.value)
         city.classList.add('is-invalid')
     else if (city.value)
         city.classList.replace('is-invalid','is-valid')
 }
 
+function areaInput(event) {
+    var area = document.getElementById('textAreaFormMid')
+    if (!area.value) 
+        area.classList.add('is-invalid')
+    else if (area.value){
+        area.classList.replace('is-invalid','is-valid')
+        sub(event)
+    }
+}
+
 function sub(event){
     var form = document.getElementById('formMid');
+    var button = document.getElementById('buttonForm')
     if (phonecheck == true) {
-        form.submit();
+        button.style.display ='inline'
+    }else{
+        
     }
 }
